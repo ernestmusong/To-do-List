6c8ffe6f584aaef2649b8d3d3448367251fa8128
Martin Ossa <martin.ossa.8@gmail.com>
2023-03-02 09:48:01 +0100

    Add delete test to test functions

--- src/todo.test.js
+++ src/todo.test.js
@@ -22,10 +22,24 @@ const addTodo = () => {
     }
   }
 
+const removeTodo =(id) => {
+     
+        const filtered = todoArray.filter((item) => item.index !== id);
+        todoArray = filtered;
+        for (let i = 0; i < todoArray.length; i += 1) {
+          todoArray[i].index = i + 1;
+        }
+
+        localStorage.setItem('todos', JSON.stringify(todoArray));
+      }
+
  describe('todo', () => {
     test('add a todo to todo array', () => {
          addTodo()
         expect(todoArray.length).toBe(1);
       });
-    
+    test('remove a todo from todo array', () => {
+         removeTodo(1)
+        expect(todoArray.length).toBe(0);
+      });
  })
