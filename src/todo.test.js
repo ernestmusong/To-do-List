06deebd415a3267b7d26b602a196aeeab8d9f254
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-02 10:11:30 +0100

    fix linters

--- src/todo.test.js
+++ src/todo.test.js
@@ -8,38 +8,36 @@ const todo = {
   completed: false,
 };
 const inputField = document.createElement('input');
-inputField.value = "one"
+inputField.value = 'one';
 
 const addTodo = () => {
-    
-    if (inputField.value.length !== 0) {
-      todo.description = inputField.value;
-      todoArray.push(todo);
-      for (let i = 0; i < todoArray.length; i += 1) {
-        todoArray[i].index = i + 1;
-      }
-      localStorage.setItem('todos', JSON.stringify(todoArray));
+  if (inputField.value.length !== 0) {
+    todo.description = inputField.value;
+    todoArray.push(todo);
+    for (let i = 0; i < todoArray.length; i += 1) {
+      todoArray[i].index = i + 1;
     }
+    localStorage.setItem('todos', JSON.stringify(todoArray));
+  }
+};
+
+const removeTodo = (id) => {
+  const filtered = todoArray.filter((item) => item.index !== id);
+  todoArray = filtered;
+  for (let i = 0; i < todoArray.length; i += 1) {
+    todoArray[i].index = i + 1;
   }
 
-const removeTodo =(id) => {
-     
-        const filtered = todoArray.filter((item) => item.index !== id);
-        todoArray = filtered;
-        for (let i = 0; i < todoArray.length; i += 1) {
-          todoArray[i].index = i + 1;
-        }
+  localStorage.setItem('todos', JSON.stringify(todoArray));
+};
 
-        localStorage.setItem('todos', JSON.stringify(todoArray));
-      }
-
- describe('todo', () => {
-    test('add a todo to todo array', () => {
-         addTodo()
-        expect(todoArray.length).toBe(1);
-      });
-    test('remove a todo from todo array', () => {
-         removeTodo(1)
-        expect(todoArray.length).toBe(0);
-      });
- })
+describe('todo', () => {
+  test('add a todo to todo array', () => {
+    addTodo();
+    expect(todoArray.length).toBe(1);
+  });
+  test('remove a todo from todo array', () => {
+    removeTodo(1);
+    expect(todoArray.length).toBe(0);
+  });
+});
