cc2f8bce9a7463006e1c59defbb4598858693263
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-11-17 23:14:52 +0100

    Remove webpack

--- src/index.js
+++ src/index.js
@@ -1,55 +1,118 @@
-/* eslint no-unused-vars: 0 */
-/* eslint import/extensions: 0 */
-import './style.css';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faRotate } from '@fortawesome/free-solid-svg-icons/faRotate';
-import { faArrowTurnDown } from '@fortawesome/free-solid-svg-icons/faArrowTurnDown';
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons/faEllipsisVertical';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons/faTrashAlt';
-import UI from './ui.js';
-import STATUS from './Status.js';
+let toDos = [];
+let todoContainer = document.querySelector('#list-wrap');
+let countEl = document.querySelector('#count');
 
-library.add(faRotate, faArrowTurnDown, faEllipsisVertical, faTrashAlt);
-dom.watch();
 
-const form = document.querySelector('#form');
-const submitBtn = document.querySelector('#submit');
-const clearBtn = document.querySelector('#clear-all');
+const displayTodos = () => {
+    let result = '';
+    let todos = JSON.parse(localStorage.getItem('todos'))
+    if(todos){
+        toDos = todos
+    }
+    toDos.map((todo) => {
+        result += `
+        <li id='' class="list-item">
+            <form id='' class='edit-todo' action="">
+                <div class="label-wrap"> 
+                     
+                     <label for="checkbox">
+                      <input onClick=showDeleteBtn(${todo.id}) class="checkbox" type="checkbox" name="checkbox" id='checkbox'>
+                     </label>
+                      <label for='todo'> 
+                    <input class="input" type="text" name="add-item" data-id="${todo.id}" value="${todo.text}">
+                </label>
+                    
+                </div>
+            </form>
+            <button onClick=showEditing(${todo.id})  class="drag edit" data-edit="${todo.id}">edit</button>
+            <button onClick=editTodo(${todo.id})  class="drag update hide" data-update="${todo.id}">update</button>
+            <button onClick=deleteTodo(${todo.id}) id="${todo.id}" class="drag hide">remove</button>
 
+        </li>
+        `
+        return result;
+    })
+    todoContainer.innerHTML = result;
+    countEl.innerHTML = toDos.length;
+    return result;
+}
+
+let todo = {
+    text: '',
+    completed: false
+}
+
+let form = document.querySelector('.form');
+let textInput = document.querySelector('#add-item');
+
+const addTodo = () => {
+   if(textInput.value.length !== 0){
+    todo.text = textInput.value
+    toDos.push(todo);
+    textInput.value = ''
+   }
+   for(let i=0; i<toDos.length; i++){
+    toDos[i].id = i + 1
+   }
+   localStorage.setItem('todos', JSON.stringify(toDos))
+   console.log(toDos)
+   displayTodos();
+}
+
+const deleteTodo = (id) => {
+    let todos = toDos.filter((item) => item.id !== id)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    displayTodos()
+    return todos;
+}
+
+const showDeleteBtn = (id) => {
+    let deleteBtn = document.getElementById(id)
+    let todo = toDos.find((todo) => todo.id === id)
+    todo.completed = !todo.completed
+    deleteBtn.classList.toggle('hide')
+}
+ 
+// Edit Todo
+const showEditing = (id) => {
+    let todo = toDos.find((todo) => todo.id === id)
+    let updateBtn = document.querySelector(`[data-update="${todo.id}"]`)
+    let editBtn = document.querySelector(`[data-edit="${todo.id}"]`)
+    let input = document.querySelector(`[data-id="${todo.id}"]`)
+    input.style.border = "1px solid red";
+    updateBtn.classList.toggle('hide')
+    editBtn.classList.toggle('hide')
+    console.log(input)
+    console.log(updateBtn)
+}
+const editTodo = (id) =>{
+    let todo = toDos.find((todo) => todo.id === id)
+    let updateBtn = document.querySelector(`[data-update="${todo.id}"]`)
+    let editBtn = document.querySelector(`[data-edit="${todo.id}"]`)
+    let input = document.querySelector(`[data-id="${todo.id}"]`)
+    if(input.value.length !== 0){
+        todo.text = input.value;
+        localStorage.setItem('todos', JSON.stringify(toDos))
+    }
+    updateBtn.classList.toggle('hide')
+    editBtn.classList.toggle('hide')
+    displayTodos()
+}
+
+
+const clearAll = () => {
+    let todos = toDos.filter((todo) => todo.completed === false)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    displayTodos()
+    return todos;
+}
+ 
 window.addEventListener('DOMContentLoaded', () => {
-  UI.displayTodos();
-});
-
-submitBtn.addEventListener('click', (e) => {
-  UI.editTodo(e);
-  UI.displayTodos();
-});
-
-window.addEventListener('click', (e) => {
-  UI.removeTodo(e);
-});
-
-window.addEventListener('change', (e) => {
-  UI.editTodo(e);
-  STATUS.checkStatus(e);
-});
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  UI.addTodo();
-  UI.displayTodos();
-});
-
-window.addEventListener('load', () => {
-  UI.displayTodos();
-});
-
-clearBtn.addEventListener('click', () => {
-  STATUS.clearAll();
-  clearBtn.style.opacity = '0.5';
-  clearBtn.style.textDecoration = 'none';
-});
-
-window.onbeforeunload = (e) => {
-  STATUS.resetCompleted();
-};
+    displayTodos();
+})
+ 
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    addTodo();
+     
+})
